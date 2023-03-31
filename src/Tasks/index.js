49d4d66c10eee6e536fa796ac5aaccd47f41247c
GI10 <tadus@op.pl>
2023-03-31 13:09:19 +0200

    changed class task__block to tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,19 +1,19 @@
 import "./style.css"
 
 const Tasks = ({ tasks, hideDoneTasks }) => (
-  <ul className="task__block">
+  <ul className="tasks">
     {tasks.map(task => (
       <li
-        className={`${task.done & hideDoneTasks ? "task__item--hide" : "task__item"}`}
+        className={`${task.done & hideDoneTasks ? "tasks__item--hide" : "tasks__item"}`}
         key={task.id}
       >
-        <button className="task__button task__button--toggleDone">
+        <button className="tasks__button tasks__button--toggleDone">
           {task.done ? "✔" : ""}
         </button>
-        <p className={`${task.done ? "task__item--done" : ""}`}>
+        <p className={`${task.done ? "tasks__item--done" : ""}`}>
           {task.content}
         </p>
-        <button className="task__button task__button--remove">
+        <button className="tasks__button tasks__button--remove">
           🗑
         </button>
       </li>
