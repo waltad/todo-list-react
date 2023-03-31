ba611fdf3ecc8b502aba7e99c469f23097ebdabb
GI10 <tadus@op.pl>
2023-03-31 12:59:03 +0200

    changed class button__navigation - buttons__button

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 const Buttons = ({ tasks, hideDoneTasks }) => (
   tasks.length > 0 && (
     <p className="buttons">
-      <button className="button__navigation">
+      <button className="buttons__button">
 				{hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
 			</button>
 			<button 
-        className={`button__navigation${tasks.every(({ done }) => done) ? " button__navigation--disabled" : ""}`}
+        className={`buttons__button${tasks.every(({ done }) => done) ? " buttons__button--disabled" : ""}`}
         disabled={tasks.every(({ done }) => done)}
       >
 				Ukończ wszystkie
