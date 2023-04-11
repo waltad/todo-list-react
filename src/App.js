1a314869eaf27874ff174fbaf4a886856ae8c3f3
GI10 <tadus@op.pl>
2023-04-11 14:22:47 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -29,7 +29,14 @@ function App() {
 
         return task;
       }));
-  }
+  };
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true
+    })));
+  };
 
   return (
     <Container>
@@ -54,6 +61,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
