4eea30765f01b6f7839264599e6518d94be7c19a
GI10 <tadus@op.pl>
2023-04-11 12:09:30 +0200

    changed autofocus to autoFocus

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
   <form className="form">
-    <input className="form__input" type="text" placeholder="Co jest do zrobienia?" autofocus required />
+    <input className="form__input" type="text" placeholder="Co jest do zrobienia?" autoFocus required />
     <button className="form__button">Dodaj zadanie</button>
   </form>
 );
