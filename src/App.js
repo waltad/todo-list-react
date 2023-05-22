15b7d7579a1650866737ffad72b78c0c9e25b2ba
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-05-22 12:31:02 +0200

    changed toggleTaskDone with use ternary operator

--- src/App.js
+++ src/App.js
@@ -20,12 +20,8 @@ function App() {
 
   const toggleTaskDone = (id) => {
     setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
-
-        return task;
-      }));
+      return task.id === id ? { ...task, done: !task.done } : task;
+    }));
   };
 
   const setAllDone = () => {
