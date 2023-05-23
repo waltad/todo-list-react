dcab4b2f013c0b8e5c678388370c3b651bfe4892
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-05-23 11:58:08 +0200

    added localStorage for tasks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
