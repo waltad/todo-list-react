0ac6ce9aea316959a5af7fd18d84b21b46123816
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-06-05 11:48:17 +0200

    removed return from toggleTaskDone

--- src/useTasks.js
+++ src/useTasks.js
@@ -1,44 +1,51 @@
 import { useState, useEffect } from "react";
 
-const getInitialTasks = (localStorageKey) => JSON.parse(localStorage.getItem(localStorageKey)) || [];
+const getInitialTasks = (localStorageKey) =>
+  JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
 export const useTasks = (localStorageKey) => {
-	
-	getInitialTasks(localStorageKey);
+  getInitialTasks(localStorageKey);
 
-	const [tasks, setTasks] = useState(getInitialTasks);
+  const [tasks, setTasks] = useState(getInitialTasks);
 
-	useEffect((localStorageKey) => {
-		localStorage.setItem(localStorageKey, JSON.stringify(tasks));
-	}, [tasks]);
+  useEffect(
+    (localStorageKey) => {
+      localStorage.setItem(localStorageKey, JSON.stringify(tasks));
+    },
+    [tasks]
+  );
 
-	const removeTask = (id) => {
-		setTasks(tasks => tasks.filter(task => task.id !== id));
-	};
+  const removeTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
 
-	const toggleTaskDone = (id) => {
-		setTasks(tasks => tasks.map(task => {
-			return task.id === id ? { ...task, done: !task.done } : task;
-		}));
-	};
+  const toggleTaskDone = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) => (
+				task.id === id ? { ...task, done: !task.done } : task
+			))
+    );
+  };
 
-	const setAllDone = () => {
-		setTasks(tasks => tasks.map(task => ({
-			...task,
-			done: true
-		})));
-	};
+  const setAllDone = () => {
+    setTasks((tasks) =>
+      tasks.map((task) => ({
+        ...task,
+        done: true,
+      }))
+    );
+  };
 
-	const addNewTask = (content) => {
-		setTasks(tasks => [
-			...tasks,
-			{
-				content,
-				done: false,
-				id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-			},
-		]);
-	};
+  const addNewTask = (content) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
 
-	return {tasks, removeTask, toggleTaskDone, setAllDone, addNewTask};
+  return { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask };
 };
