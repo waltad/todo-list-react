1b19afc55b7437eafde5d9215e6aab7a0c20ff6a
Tadek Waluga <tadek.waluga@gmail.com>
2023-06-27 20:44:31 +0200

    removed unused exports

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,34 +4,19 @@ import Buttons from "./Buttons";
 import Section from "./../../common/Section";
 import Header from "./../../common/Header";
 import Container from "./../../common/Container";
-// import { useState } from "react";
 import { useTasks } from "../../useTasks";
-// import { useSelector } from "react-redux";
-// import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-  // const [hideDone, setHideDone] = useState(false);
-
-  // const { tasks } = useSelector(selectTasks);
-
   const {
-    // tasks,
-    // removeTask,
-    // toggleTaskDone,
     setAllDone,
-    addNewTask,
   } = useTasks();
-
-  // const toggleHideDone = () => {
-  //   setHideDone((hideDone) => !hideDone);
-  // };
 
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
 
       <Section
