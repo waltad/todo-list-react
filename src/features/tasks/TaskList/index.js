4b76dae0d71317899e8ec2b885d0337152eabee3
Tadek Waluga <tadek.waluga@gmail.com>
2023-06-27 20:23:23 +0200

    added removeTask using Redux

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleTaskDone } from "../tasksSlice";
+import { selectTasks, toggleTaskDone, removeTask } from "../tasksSlice";
 import { List, Item, Content, Button } from "./styled";
 
-const TaskList = ({ removeTask }) => {
+const TaskList = () => {
   const { tasks, hideDone } = useSelector(selectTasks);
   const dispatch = useDispatch();
 
@@ -25,7 +25,7 @@ const TaskList = ({ removeTask }) => {
           </Content>
           <Button
             remove
-            onClick={() => removeTask(task.id)}
+            onClick={() => dispatch(removeTask(task.id))}
           >
             🗑
           </Button>
--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -17,9 +17,13 @@ const tasksSlice = createSlice({
       const index = state.tasks.findIndex(task => task = task.id === action.payload);
       state.tasks[index].done = !state.tasks[index].done;
     },
+    removeTask: (state, action) => {
+      const index = state.tasks.findIndex(task => task = task.id === action.payload);
+      state.tasks.splice(index, 1);
+    },
   },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
